d1043f926517461245bc09d80595d1b1a70bfa62
Anna Doshchenko <annadoshenko3@gmail.com>
2020-10-05 15:25:35 +0300

    Constants change

--- src/irecommend.py
+++ src/irecommend.py
@@ -4,11 +4,10 @@ import csv
 import time
 import random
 
-page = 1
-max_page = 80
-rating = 5
-site_id = 7211
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 OPR/71.0.3770.171"
+MAX_PAGE = 80
+RATING = 5
+SITE_ID = 7211
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 OPR/71.0.3770.171"
 
 
 def parse_div(div):  # Функция разбора таблицы с вопросом
